0a6ce322ba9169e8c8c601b961b8ce1dbc42307f
uditdotcom <uditgupta8595@gmail.com>
2025-06-07 23:43:27 +0530

    Update config.js
    extra info

--- backend/config.js
+++ backend/config.js
@@ -1,5 +1,6 @@
 export const PORT =3000;
 
-export const mongoDBURL = 'mongodb+srv://udit9315gupta:80085@books-store-mern.jhxgc.mongodb.net/?retryWrites=true&w=majority&appName=Books-Store-MERN';
+export const mongoDBURL = '??'; // write your mongo cluster url
+
 //PLEASE CREATE A FREE DATABASE FOR YOURSELF.
 //THIS
